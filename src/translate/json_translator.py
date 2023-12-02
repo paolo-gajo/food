bd7eef43257f46b1b4bba0de631b6b1c8a99f5ed
paolo-gajo <paolo.gajo@studio.unibo.it>
2023-12-02 21:42:12 +0100

    remove duplicate file

--- src/translate/json_translator.py
+++ src/translate/json_translator.py
@@ -2,6 +2,8 @@ import json
 from tqdm.auto import tqdm
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import torch
+import time
+import os
 
 # Pseudo code:
     # 1. look at each element in 'entities', e.g. [0, 1, 'QUANTITY'], and find the equivalent to the positions in 'text'
@@ -25,6 +27,9 @@ def translate_marianmt(text, tokenizer, model, device):
     generated_ids = model.generate(**batch, max_new_tokens=1024)
     return tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
 
+def has_alphanumeric(s):
+    return any(char.isalnum() for char in s)
+
 def translate_TASTEset(json_path, tokenizer, model, device, verbose=False):
     """
     Translate the annotations within a TASTEset JSON file.
@@ -36,18 +41,19 @@ def translate_TASTEset(json_path, tokenizer, model, device, verbose=False):
     - device: The device to run the model on.
     - verbose: If set to True, prints additional information.
     """
+
     with open(json_path, encoding='utf8') as json_file:
         data = json.load(json_file)
 
-    new_data = {'annotations': data['annotations']}
+    new_data = {'annotations': data['annotations'][:5]}
 
     # Process each recipe's annotations
     for recipe in tqdm(new_data['annotations'], desc="Translating"):
         new_text = ''
         entities_trg = []
         shift = 0
-
         for entity in recipe['entities']:
+            print('-------------------')
             start_src, end_src, entity_type = entity
             entity_text_src = recipe['text'][start_src:end_src]
             if verbose > 1:
@@ -58,14 +64,37 @@ def translate_TASTEset(json_path, tokenizer, model, device, verbose=False):
 
             shift += len(entity_text_trg) - len(entity_text_src)
 
-            
-
             new_text += entity_text_trg
+
             end_trg = len(new_text)
-            new_text += recipe['text'][end_src:end_src+1]
-            entities_trg.append([start_trg, end_trg, entity_type])
+
             if verbose > 1:
                 print('target:', new_text[start_trg:end_trg], start_trg, end_trg)
+
+            if recipe['text'][end_src:end_src+1] == ';':
+                end_char_trg = recipe['text'][end_src:end_src+1]
+            else:
+                # look for the next ; in the source text
+                # if there are any alphanumeric characters before it
+                # then end_char = ' ', else end_char = '; '
+                end_char_src = recipe['text'][end_src:].find(';')
+                if end_char_src == -1:
+                    # -1 means no ; was found
+                    end_char_trg = recipe['text'][end_src:end_src+1]
+                else:
+                    end_char_src += end_src
+                    print("recipe['text'][end_src:end_char_src]", recipe['text'][end_src:end_char_src])
+                    if has_alphanumeric(recipe['text'][end_src:end_char_src]):
+                        end_char_trg = recipe['text'][end_src:end_src+1]
+                    else:
+                        end_char_trg = '; '
+                    
+            print('end_char_trg: "', end_char_trg, '"') if end_char_trg != ' ' else print('end_char_trg: <space>')
+            
+            new_text += end_char_trg
+
+            entities_trg.append([start_trg, end_trg, entity_type])
+            
 
         if verbose:
             print('original text:', recipe['text'])
@@ -75,7 +104,9 @@ def translate_TASTEset(json_path, tokenizer, model, device, verbose=False):
 
     # Output the new data
     print('new_data:', new_data)
+    time_string = time.strftime("%Y%m%d-%H%M%S")
     output_path = json_path.replace('.json', '_translated.json')
+    output_path = os.path.join(os.path.dirname(output_path), time_string + os.path.basename(output_path))
     with open(output_path, 'w', encoding='utf-8') as outfile:
         json.dump(new_data, outfile, indent=4, ensure_ascii=False)
     print(f"Translated data saved to {output_path}")
@@ -89,7 +120,7 @@ def main():
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name).to(device)
 
-    json_path = '/home/pgajo/working/food/TASTEset/data/TASTEset_semicolon.json'
+    json_path = '/home/pgajo/working/food/data/TASTEset/data/TASTEset_semicolon.json'
     translate_TASTEset(json_path, tokenizer, model, device, verbose=2)
 
 if __name__ == '__main__':
