cd53ad4c40866264f48d228c4c6f879ab0a39864
paolo-gajo <paolo.gajo2@unibo.it>
2024-01-29 19:20:23 +0100

    fix metrics markdown in model card pushing

--- src/word_alignment/train.py
+++ src/word_alignment/train.py
@@ -8,6 +8,7 @@ from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 from utils import data_loader, SquadEvaluator, TASTEset, XLWADataset, push_model_repo_to_hf, save_local_model, push_card
 from datetime import datetime
 from huggingface_hub import HfApi
+import pandas as pd
 
 def main():
     model_name = 'bert-base-multilingual-cased'
@@ -44,7 +45,7 @@ def main():
             unshuffled_size = 1,
             # drop_duplicates = False,
             debug_dump = True,
-            # n_rows=200,
+            n_rows=200,
             )
 
     # data_path = f'//home/pgajo/working/food/data/XL-WA/data'
@@ -58,7 +59,7 @@ def main():
     
     # data = DatasetDict.load_from_disk(data_path) # load prepared tokenized dataset
     # print('max_num_tokens', [len(data['train'][i]['input_ids']) for i in range(0)])
-    batch_size = 32
+    batch_size = 8
     dataset = data_loader(data,
                         batch_size,
                         )
@@ -80,7 +81,7 @@ def main():
                             load("squad_v2"),
                             )
 
-    epochs = 10
+    epochs = 1
 
     for epoch in range(epochs):
         # train
@@ -157,37 +158,41 @@ def main():
     # model save folder
     model_dir = './models'
     save_name = f"{model_dict[model_name]}_{data.name}_U{data.unshuffled_size}_S{data.shuffled_size}_E{evaluator.epoch_best}_DEV{str(round(evaluator.exact_dev_best, ndigits=0))}_DROP{str(int(data.drop_duplicates))}"
+    save_name = save_name + "_test" # comment if not testing
     model_save_dir = os.path.join(model_dir, f"{data.name}/{save_name}")
     if not os.path.isdir(model_save_dir):
         os.makedirs(model_save_dir)
 
     evaluator.save_metrics_to_csv(os.path.join(model_save_dir, 'metrics'))
     save_local_model(model_save_dir, model, tokenizer)
-    
-    model_description = f'''
-    Model: {model_dict[model_name]}
-    Dataset: {data.name}
-    Unshuffled ratio: {data.unshuffled_size}
-    Shuffled ratio: {data.shuffled_size}
-    Best exact match epoch: {evaluator.epoch_best}
-    Best exact match: {str(round(evaluator.exact_dev_best, ndigits=2))}
-    Drop duplicates: {data.drop_duplicates}
-    Optimizer lr = {lr}
-    Optimizer eps = {eps}
-    Batch size = {batch_size}
-    '''
-    open(f'{model_save_dir}/model_description.txt', 'w', encoding='utf8').write(model_description)
 
     repo_id = f"pgajo/{save_name}"
 
     api = HfApi()
     token = os.environ['HF_TOKEN']
     api.create_repo(repo_id, token=token)
+
+    model_description = f'''
+    Model: {model_dict[model_name]}\n
+    Dataset: {data.name}\n
+    Unshuffled ratio: {data.unshuffled_size}\n
+    Shuffled ratio: {data.shuffled_size}\n
+    Best exact match epoch: {evaluator.epoch_best}\n
+    Best exact match: {str(round(evaluator.exact_dev_best, ndigits=2))}\n
+    Drop duplicates: {data.drop_duplicates}\n
+    Optimizer lr = {lr}\n
+    Optimizer eps = {eps}\n
+    Batch size = {batch_size}\n
+    Metrics:\n
+    {pd.DataFrame(evaluator.metrics).round(2).to_markdown()}
+    '''
+    open(f'{model_save_dir}/model_description.txt', 'w', encoding='utf8').write(model_description)
     push_card(repo_id=repo_id,
             model_name=model_name,
             model_description=model_description,
             )
-    api.upload_folder(repo_id=repo_id, folder_path=model_save_dir, token=token)
+    
+    api.upload_folder(repo_id=repo_id, folder_path=model_save_dir, token=token) 
 
 if __name__ == '__main__':
     with warnings.catch_warnings():
